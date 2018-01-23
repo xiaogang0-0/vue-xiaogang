2a0577e53378580b48ff67a4f27c62bc7473e8e9
binge0-0 <361204134@qq.com>
2018-01-23 18:30:05 +0800

    'vue-xiaogang'

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <!-- <img src="./assets/home-bg01.jpg"> -->
+    <!-- <img src="./assets/logo.png"> -->
+    <router-view/>
+    <p>我就是后添加的底</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  background: url(./assets/home-bg01.jpg) no-repeat; 
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+p{ line-height: 50px; }
+</style>
