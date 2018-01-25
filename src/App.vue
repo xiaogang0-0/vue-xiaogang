d10f8da582617cf0e1b6c7634d28d2edbdaba25f
binge0-0 <361204134@qq.com>
2018-01-25 16:56:39 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,8 @@
     <!-- <img src="./assets/home-bg01.jpg"> -->
     <!-- <img src="./assets/logo.png"> -->
     <router-view/>
-    <p>我就是后添加的底为啥</p>
+
+    <p>my is add bottom</p>
   </div>
 </template>
 
@@ -23,6 +24,6 @@ export default {
   margin-top: 60px;
   background:url("./assets/home-bg01.jpg") no-repeat 0 0;
 }
-p{ line-height: 40px; color: #ccc; }
+p{ line-height: 40px; color: rgba(0,0,0,0.2); }
 
 </style>
