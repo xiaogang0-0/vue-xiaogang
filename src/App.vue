d58830251b6b2292fe6b3d8e825c34b71adfa0fd
binge0-0 <361204134@qq.com>
2018-01-27 09:27:54 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -7,8 +7,8 @@
             <template slot="title"><i class="el-icon-message"></i>导航一</template>
             <el-menu-item-group>
               <template slot="title">分组一</template>
-              <el-menu-item index="1-1">选项0</el-menu-item>
-              <el-menu-item index="1-2">选项1</el-menu-item>
+              <el-menu-item index="1-1">选项1</el-menu-item>
+              <el-menu-item index="1-2">选项2</el-menu-item>
             </el-menu-item-group>
             <el-menu-item-group title="分组2">
               <el-menu-item index="1-3">选项3</el-menu-item>
